dff867743956f5f53fe551e310f1bd0370051ca5
Jay Tang/WHQ/Wistron <Jay_Tang@wistron.com>
2022-09-21 14:23:01 +0800

    cartItems counter

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,11 +1,12 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 export const CartContext = createContext(
     {
         isCartOpen: false,
         setIsCartOpen: () => {},
         cartItems: [], //購物車內容
-        addItemToCart: () => {} //加物品到購物車
+        addItemToCart: () => {}, //加物品到購物車
+        cartCount: 0 //購物車內有多少產品 for context
     }
 )
 
@@ -32,12 +33,22 @@ const addCartItem = (cartItems, productToAdd) => {
 export const CartProvider = ({children}) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([]);
+    const [cartCount, setCartCount] = useState(0); //購物車內有多少產品 for component 
+
+    //查看是否需要re-render cartCount
+    useEffect(() => {
+        //使用 js reduce funciton 來累加 cartCount
+        const newCartCount = cartItems.reduce(
+            (total, cartItem) => total + cartItem.quantity, 0
+        );
+        setCartCount(newCartCount);
+    }, [cartItems]);
 
     const addItemToCart = (productToAdd) => {
         setCartItems(addCartItem(cartItems, productToAdd))
     };
 
-    const value = {isCartOpen, setIsCartOpen, cartItems, addItemToCart};
+    const value = {isCartOpen, setIsCartOpen, cartItems, addItemToCart, cartCount};
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
