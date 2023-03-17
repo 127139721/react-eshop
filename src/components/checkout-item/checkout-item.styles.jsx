a8d51ab1c442c293b2d64ed23cc2d6ea24e39179
Jay Tang/WHQ/Wistron <Jay_Tang@wistron.com>
2023-03-17 14:51:27 +0800

    MUI checkout-item finished

--- src/components/checkout-item/checkout-item.styles.jsx
+++ src/components/checkout-item/checkout-item.styles.jsx
@@ -17,36 +17,36 @@ export const CheckoutItemContainer = MUIStyled("div")(({ theme }) => ({
   }
 }));
 
-export const ImageContainer = styled.div`
-  width: 23%;
-  padding-right: 15px;
-  img {
-    width: 100%;
-    height: 100%;
+export const ImageContainer = MUIStyled("div")({
+  width: "23%",
+  paddingRight: "15px",
+  img :{
+    width: "100%",
+    height: "100%"
   }
+});
 
-`;
-
-export const BaseSpan = styled.span`
-  width: 23%;
-  @media screen and (max-width: 800px) {
-    width: 22%;
+export const BaseSpan = MUIStyled("span")(({ theme }) => ({
+  width: "23%",
+  /* md 以下會套用 */
+  [theme.breakpoints.down("md")]: {
+    width: "22%",
   }
-`;
+}));
 
-export const Quantity = styled(BaseSpan)`
-  display: flex;
-`;
+export const Quantity = MUIStyled(BaseSpan)({
+  display: "flex"
+});
 
-export const Arrow = styled.div`
-  cursor: pointer;
-`;
+export const Arrow = MUIStyled("div")({
+  cursor: "pointer"
+});
 
-export const Value = styled.span`
-  margin: 0 10px;
-`;
+export const Value = MUIStyled("span")({
+  margin: "0 10px",
+});
 
-export const RemoveButton = styled.div`
-  padding-left: 12px;
-  cursor: pointer;
-`;
+export const RemoveButton = MUIStyled("div")({
+  paddingLeft: "12px",
+  cursor: "pointer",
+});
